fe6db4f262a8d8459bb8cd93b7fa15e43695b035
fleischer <sean.fleischer@comsysto.com>
2017-10-10 19:15:18 +0200

    [UPDATE] - Added method to process frontend configuration changes

--- transformer.rb
+++ transformer.rb
@@ -8,6 +8,19 @@ require 'rest_client'
 sid = 0
 http = Net::HTTP.new('http://traefik.ecf.prototyp.it', 18080)
 
+def updateFrontend(currentConfiguration, routeUpdate)
+  uris = routeUpdate['uris']
+  frontendConfigurations = currentConfiguration['frontends']
+  frontendConfigurations.each do |key, value|
+    if key == uris[0] + '-fe'
+      puts frontendConfigurations[key]['routes']
+      #frontendConfigurations[key]['routes']['route1']['rule'] = 'Host: ' + uris[0]
+    end
+  end
+  puts 'Frontend DONE'
+end
+
+
 if ARGV.length>0
   natsEndpoints = ARGV[0].split(',')
 else
@@ -22,9 +35,9 @@ NATS.start(:servers => natsEndpoints) do
   sid = NATS.subscribe('router.register') {|msg|
     puts "Msg received : '#{msg}'"
     routeMsg = JSON.parse(msg)
-    puts "dest IP: '#{routeMsg["host"]}'"
-    puts "dest Port: '#{routeMsg["port"]}'"
-    routeMsg["uris"].each do |uri|
+    puts "dest IP: '#{routeMsg['host']}'"
+    puts "dest Port: '#{routeMsg['port']}'"
+    routeMsg['uris'].each do |uri|
       puts "from #{uri}"
     end
 
@@ -32,15 +45,18 @@ NATS.start(:servers => natsEndpoints) do
     traefikWeb = JSON.parse(httpResp.body)
     puts traefikWeb
 
+    updateFrontend(traefikWeb, routeMsg)
+
     newServer         = {}
     newServer[:url]  = 'localhost:8080'
     newServer[:weight] = 0
-    traefikWeb["backends"]["back-cf-production"]["servers"]["server2"] = newServer
+    traefikWeb['backends']['back-cf-production']['servers']['server2'] = newServer
 
-    http.request_put('/api/providers/web', JSON.generate(newServer))
+    #http.request_put('/api/providers/web', JSON.generate(newServer))
+
 
     #backend block
   }
-  
+
 end
 
